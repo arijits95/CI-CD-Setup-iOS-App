36b790c2de1c1e0284218b10a4dbfd4f6f31851e
Arijit Sarkar <arijits95@gmail.com>
2026-01-20 11:46:18 +0530

    Empty project pushed

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Arijit Sarkar on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleApp/SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by Arijit Sarkar on 20/01/26.
+//
+
+import Testing
+@testable import SampleApp
+
+struct SampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
